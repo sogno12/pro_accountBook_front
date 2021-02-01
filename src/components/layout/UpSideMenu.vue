ed811186be3fe0abe3ed7c9be57e0bf1dc786d55
sjchoi <sjchoi@redsoft.co.kr>
2021-02-01 17:58:36 +0900

    sideMenu 메뉴 리스트화

--- src/components/layout/UpSideMenu.vue
+++ src/components/layout/UpSideMenu.vue
@@ -1,5 +1,22 @@
 <template>
-  <v-navigation-drawer permanent> 네비 </v-navigation-drawer>
+  <v-navigation-drawer permanent>
+    <!-- 메뉴 -->
+    <v-list>
+      <v-list-group
+        v-for="menu in menus"
+        :key="menu.menuId"
+        :prepend-icon="menu.menuIcon"
+      >
+        <v-list-item
+          v-for="child in menu.children"
+          :key="child.menuId"
+          :to="RX_PAGE[child.menuId]"
+        >
+          <v-list-item-title v-text="child.menuName"> </v-list-item-title>
+        </v-list-item>
+      </v-list-group>
+    </v-list>
+  </v-navigation-drawer>
 </template>
 
 <script>
@@ -14,8 +31,25 @@ export default {
   methods: {
     async getMenu() {
       try {
-        console.log("get menu")
-        await $getMenu();
+        console.log("get menu");
+        const { data } = await $getMenu();
+        const menus = data.result;
+        console.log("menus", menus);
+
+        // parantMenus 별로 나누기 (root)
+        let parentMenus = menus.filters((menu) => {
+          return menu.upMenuId === "root";
+        });
+
+        // childrenMenus 나누기
+        parentMenus.forEach((parentMenu) => {
+          let childrenMenu = menus.filters((menu) => {
+            return menu.upMenuId === parentMenu.menuId;
+          });
+          parentMenu.children = childrenMenu;
+        });
+
+        this.menus = parentMenus;
       } catch (error) {
         console.log("menu", error);
       }
